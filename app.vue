7beb4d6f3ba5d332befd919e8515901e91f44fbf
flavioinform <flavi0camp0dev@gmail.com>
2025-12-16 19:03:29 -0300

    comiit del titulo falta icono

--- app.vue
+++ app.vue
@@ -1,4 +1,6 @@
 <template>
+  <title>Eco-Infinity</title>
+  <meta name="description" content="Eco-Infinity es una startup líder en servicios y ventas de tecnologías. Innovación y Sustentabilidad integradas para el futuro." />
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
